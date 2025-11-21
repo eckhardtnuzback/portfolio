730f7bb4e68c8e39703bbf8f50ff914c73475719
eckhardtnuzback <eckhardtnuzback@gmail.com>
2025-11-20 16:48:46 -0800

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -21,7 +21,7 @@ const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>v>
+    <Router basename={process.env.PUBLIC_URL}>
       <ScrollToTop>
         <Headermain />
         <AppRoutes />
